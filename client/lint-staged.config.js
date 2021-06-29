d07bc5ded174a3f7a88f7ad998e30a3565dd50d6
Amsavarthan Lv <80940598+amsavarthan@users.noreply.github.com>
2021-06-29 14:34:15 +0530

    Restructured directories 🗂

--- client/lint-staged.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  // Run type-check on changes to TypeScript files
-  "*.{js,ts,tsx}": () => "eslint --quiet --fix",
-  // Run ESLint on changes to JavaScript/TypeScript files
-  "*.{json,md,html})": () => `prettier --write`,
-};
--- client/truffle/truffle-config.js
+++ client/truffle/truffle-config.js
@@ -3,24 +3,21 @@ require('dotenv').config();
 
 const mnemonic = process.env.MNEMONIC;
 const infura_endpoint = process.env.INFURA_ENDPOINT;
-const path = require("path")
+const path = require('path');
 
 module.exports = {
-
-    contracts_build_directory: path.join(__dirname, "../client/src/contracts"),
+    contracts_build_directory: path.join(__dirname, '../src/contracts'),
     networks: {
-
         development: {
-            host: "127.0.0.1",
+            host: '127.0.0.1',
             port: 8545,
-            network_id: "*",
+            network_id: '*',
         },
 
         rinkeby: {
             provider: () => new HDWalletProvider(mnemonic, infura_endpoint),
             network_id: 4,
-            skipDryRun: true
+            skipDryRun: true,
         },
-
-    }
+    },
 };
